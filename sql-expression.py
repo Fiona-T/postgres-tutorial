13f2a4abe6669fd0bdccf996406c8ede751c1063
Fiona-T <fitracey@gmail.com>
2021-09-29 10:20:07 +0000

    add comments to sql-expression.py

--- sql-expression.py
+++ sql-expression.py
@@ -1,12 +1,14 @@
+# import classes from sqlalchemy module
 from sqlalchemy import (
     create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
 )
 
-# link Python file to chinook db
+# link Python file to chinook db using create_engine component
+# Tell it to point to local chinook db on the postgress server
 # engine created and connected to the local database
 db = create_engine("postgresql:///chinook")
 
-# data about the table objects and the data in them
+# data about the table objects and the data in them using MetaData class
 meta = MetaData(db)
 
 # create variable for Artist table - table class/data model
@@ -26,6 +28,8 @@ album_table = Table(
 )
 
 # variable for Track table in db
+# foreign keys MediaType and GenreId are linked to tables we are not using, so
+# just setting primary_key=False instead
 track_table = Table(
     "Track", meta,
     Column("TrackId", Integer, primary_key=True),
@@ -41,14 +45,21 @@ track_table = Table(
 
 # connect to db, save connection into variable called connection
 with db.connect() as connection:
+
+    # use the select method on the table
+
     # query 1 - select all records from the Artist table
     # select_query = artist_table.select()
 
     # query 2 - select only the Name column from the Artist table
-    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+    # with_only_columns: specify table, then .c. for column name,
+    # all inside list[]
+    # select_query = artist_table.select().with_only_columns(
+    #   [artist_table.c.Name])
 
     # query 3 - select only Queen from the Artist table
-    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+    # select_query = artist_table.select().where(
+    #   artist_table.c.Name == "Queen")
 
     # query 4 - select only ArtistId 51 from the Artist table
     # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
@@ -56,9 +67,13 @@ with db.connect() as connection:
     # query 5 - select only the albums with ArtistId 51 from the Album table
     # select_query = album_table.select().where(album_table.c.ArtistId == 51)
 
-    # query 6 - select all tracks where the composer is Queen from the Track table
-    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+    # query 6 - select all tracks where composer is Queen from the Track table
+    select_query = track_table.select().where(
+        track_table.c.Composer == "Queen")
 
+    # use the execute method on the db connection to run the query
     results = connection.execute(select_query)
+
+    # print each result in the results variable
     for result in results:
         print(result)
